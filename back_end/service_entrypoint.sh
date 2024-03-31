49fba979bde05d985398a7e15bef910e0d789fb8
GaboSO21 <gso2090@gmail.com>
2024-03-31 14:34:43 -0600

    instrucciones

--- back_end/service_entrypoint.sh
+++ back_end/service_entrypoint.sh
@@ -12,6 +12,8 @@
 # GRANT ALL PRIVILEGES ON DATABASE cool_db TO "cool-app";
 # GRANT CONNECT ON DATABASE cool_db TO "cool-app";
 # GRANT USAGE, CREATE ON SCHEMA public TO "cool-app";
+# insert into rol values (1, 'ADMIN');
+# insert into usuario values ('305430692', 'Gabriel', 'Sanchez', 'Olveira', 'gso2090@gmail.com', '6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090', 1);
 
 sleep 10
 flask --app index.py db migrate
